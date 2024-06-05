f41a1585e09a7dc6082a7ad3cb5bd77eccf612db
kabiiir23 <k@markopolo.ai>
2024-06-05 12:33:50 +0600

    Updated env validation schema

--- libs/core/src/config/config.options.ts
+++ libs/core/src/config/config.options.ts
@@ -9,6 +9,11 @@ const ConfigOptions = {
       .default('development'),
     PORT: Joi.number().default(3000),
     MONGO_URI: Joi.string().required(),
+    MONGO_DB_NAME: Joi.string().required(),
+    APP_NAME: Joi.string(),
+    INJECT_CID: Joi.boolean().default(false),
+    LOGGER_MIN_LEVEL: Joi.string().default('debug'),
+    LOGGER_DISABLE: Joi.boolean().default(false),
   }),
 };
 
